e9e6755b30f3e856f3d04ace347bc888f2e15cf3
Sean Melton <seanlape@gmail.com>
2020-03-12 18:11:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,11 +24,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest
+shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-shipping_manifest[]
+shipping_manifest["gun powder"] = 4
 shipping_manifest
 
   # return the shipping_manifest hash below
